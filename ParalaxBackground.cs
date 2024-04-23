a087711d6f1676c97576024e001d57625aab6200
HaikalDev27 <72543199+HaikalDev27@users.noreply.github.com>
2024-04-23 23:34:52 +0700

    Fixing Paralax by @HaikalDev27

--- ParalaxBackground.cs
+++ ParalaxBackground.cs
@@ -4,32 +4,24 @@ using UnityEngine;
 
 public class ParalaxBackground : MonoBehaviour
 {
-    [SerializeField] private Vector2 paralaxEffectMultiplier;
+    public float parallaxSpeed = 0.1f; // Kecepatan pergerakan paralaks
 
-    private Transform cameraTransform;
-    private Vector3 lastCameraPosition;
-    private float textureUnitSizeX;
+    private Vector2 startPosition; // Posisi awal latar belakang
 
-    // Start is called before the first frame update
     void Start()
     {
-        cameraTransform = Camera.main.transform;
-        lastCameraPosition = cameraTransform.position;
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        Texture2D texture = sprite.texture;
-        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;   
+        startPosition = transform.position; // Simpan posisi awal latar belakang
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * paralaxEffectMultiplier.x, deltaMovement.y * paralaxEffectMultiplier.y);
-        lastCameraPosition = cameraTransform.position;
+        // Hitung pergerakan paralaks berdasarkan waktu
+        float parallaxOffset = Time.time * parallaxSpeed;
 
-        if(Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX){
-            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
-        }
+        // Atur posisi latar belakang berdasarkan offset paralaks
+        Vector2 newPosition = startPosition + Vector2.right * parallaxOffset;
+
+        // Terapkan posisi baru ke latar belakang
+        transform.position = newPosition;
     }
 }
