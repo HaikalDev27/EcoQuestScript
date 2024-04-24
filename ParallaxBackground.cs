814ae15e4e6926da6f42d0df389659ed3ec0ffd5
HaikalDev27 <72543199+HaikalDev27@users.noreply.github.com>
2024-04-24 18:41:25 +0700

    Adding Parallax Scheme by @HaikalDev27

---
+++ ParallaxBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+
+	private float length, startpos;
+	public GameObject cam;
+	public float parallaxSpeed;
+
+	// Use this for initialization
+	void Start () {
+		startpos = transform.position.x;
+		length = GetComponent<SpriteRenderer> ().bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = (cam.transform.position.x * parallaxSpeed);
+
+		transform.position = new Vector3 (startpos + dist, transform.position.y, transform.position.z);
+	}
+}
