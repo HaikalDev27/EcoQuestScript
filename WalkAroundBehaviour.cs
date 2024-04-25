fb289eef09e7168b389888c120cc2660734c5f92
HaikalDev27 <72543199+HaikalDev27@users.noreply.github.com>
2024-04-26 04:16:15 +0700

    Fixing Behaviour

--- WalkAroundBehaviour.cs
+++ WalkAroundBehaviour.cs
@@ -7,6 +7,11 @@ public class WalkAroundBehaviour : MonoBehaviour
     public float moveSpeed = 3f; // Kecepatan berjalan
     public Transform[] waypoints; // Titik-titik untuk bergerak
     private int currentWaypoint = 0; // Indeks waypoint saat ini
+    private SpriteRenderer spriteRenderer;
+
+    void Start(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
@@ -17,6 +22,12 @@ public class WalkAroundBehaviour : MonoBehaviour
         if (Vector2.Distance(transform.position, waypoints[currentWaypoint].position) < 0.1f)
         {
             currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        }
+
+        if(rb.velocity.x > -1){
+            spriteRenderer.flipX = true;
+        } else  {
+            spriteRenderer.flipX = false;
         }
     }
 }
