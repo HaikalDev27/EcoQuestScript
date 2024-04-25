20dd819d381d6397f167184e22455969b657a408
HaikalDev27 <72543199+HaikalDev27@users.noreply.github.com>
2024-04-25 23:42:28 +0700

    Missing Error1

--- InteractButton.cs
+++ InteractButton.cs
@@ -8,7 +8,7 @@ public class InteractButton : MonoBehaviour
     // Fungsi yang dipanggil saat tombol UI di klik
     public void OnButtonClick()
     {
-        Input.SetButtonDown("Fire1");
+        InputManager.Instance.SetButtonDown("Fire1");
         Debug.LogWarning("Error woy!! Udah tidur ae sono goblok");
     }
 }
