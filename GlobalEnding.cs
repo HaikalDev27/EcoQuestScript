d30b8af2f0c46097e8d289eee521a0e7380a660e
HaikalDev27 <72543199+HaikalDev27@users.noreply.github.com>
2024-04-26 13:27:09 +0700

    Ending Added

--- GlobalEnding.cs
+++ GlobalEnding.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class GlobalEnding : MonoBehaviour
 {
     public string EndTheme;
+    public UnityEvent Action;
+
     // Start is called before the first frame update
     void Start()
     {
         FindFirstObjectByType<AudioManager>().Stop("MainMenuTheme");
         FindFirstObjectByType<AudioManager>().Stop("MainTheme");
         FindFirstObjectByType<AudioManager>().Play(EndTheme);
+        Action.Invoke();
     }
 
     // Update is called once per frame
